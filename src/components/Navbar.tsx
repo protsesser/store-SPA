1d97bf0b67343b5fadbf179002eeda6699aef38b
Сергей Процай <spro1103@mail.ru>
2024-06-07 19:12:13 +0300

    overlay on items done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,11 @@ const NavBar: React.FC = () => {
           <div className="flex ">
             <h2>Logo</h2>
           </div>
-          <div className="flex-1 flex gap-8 justify-end ml-auto">
+          <div className="flex-1 flex gap-8 justify-end ml-auto items-center">
             <h2>Login</h2>
-            <h2>Cart</h2>
+            <button className="bg-sky-300 h-12 w-12 flex items-center justify-center rounded-2xl shadow-lg">
+              <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="m4.05108 8.9199c.04175-.51955.47556-.9199.99679-.9199h13.90423c.5213 0 .9551.40035.9968.9199l.8775 10.9199c.0935 1.164-.8258 2.1602-1.9936 2.1602h-13.66564c-1.16773 0-2.08711-.9962-1.99357-2.1602z"/><path d="m16 11v-5c0-2.20914-1.7909-4-4-4-2.20914 0-4 1.79086-4 4v5"/></g></svg>
+            </button>
           </div>
         </div>
       </nav>
--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -7,7 +7,7 @@ interface ProductItemProps {
 
 const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
     return (
-        <div className = "w-full bg-white overflow-y-hidden rounded-2xl shadow-lg">
+        <div className = "relative w-full bg-white overflow-y-hidden rounded-2xl shadow-lg group">
             <div className="p-4 grid grid-rows-4 justify-center h-full w-full" >
                 <div className=" row-span-3 flex items-center justify-center">
                     <img src={product.image} alt={product.title} className="h-full w-full object-contain "/>
@@ -16,6 +16,11 @@ const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
                     <h2 className="flex-1">{product.title}</h2>
                     <p className="text-right min-w-24">${product.price}</p>
                 </div>
+            </div>
+            <div className=" absolute h-full w-full bg-black/60 flex flex-col items-center justify-center text-white -bottom-0 blur-sm group-hover:blur-0 opacity-0 group-hover:opacity-100 transition-all duration-150">
+                <h1 className="mx-4 text-2xl text-center font-bold">{product.title}</h1>
+                <p className="mx-4 text-justify my-8 h-64 overflow-auto">{product.description}</p>
+                <button className="bg-sky-300 text-black rounded-xl py-2 px-4">Add to cart</button>
             </div>
         </div>
     );
